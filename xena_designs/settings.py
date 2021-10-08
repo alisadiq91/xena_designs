d87bfa8ffd7eee772fc2ab43bb0acac5159eefc5
alisadiq91 <ali.sadiq91@gmail.com>
2021-10-08 14:53:01 +0000

    added functionality to checkout app

--- xena_designs/settings.py
+++ xena_designs/settings.py
@@ -175,4 +175,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
