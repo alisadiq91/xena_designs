f8da0c1d42aacde58047854a3d0e197d573fcd0f
alisadiq91 <ali.sadiq91@gmail.com>
2021-10-25 21:20:18 +0000

    removed secret key and set debug

--- xena_designs/settings.py
+++ xena_designs/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mwr-yz5j81^v2_*p-0sd0peivqkx9(!d2$v#8k^fh6c*1$9yaa'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['xena-designs.herokuapp.com', 'localhost']
 
