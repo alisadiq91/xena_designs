821c5e6803421a6d238ed378781c494cd53f3804
alisadiq91 <ali.sadiq91@gmail.com>
2021-11-21 22:24:42 +0000

    added review carousel to homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from reviews.models import Review
 # Create your views here.
 
 def index(request):
     """ View to return the index page """
     return render(request, 'home/index.html')
+
+
+def reviews_carousel(request):
+    """ A view to return the reviews to carousel """
+    review = Review.objects.order_by('-date_created')
+    context = {
+        'review': review
+    }
+    return render(request, template, context)
 
